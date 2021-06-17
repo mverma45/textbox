c3474041b7f1179a1c045cbec87f34a3d897d305
Manendar Verma <mverma7845@gmail.com>
2021-06-17 14:31:55 -0600

    updating js file
    adding a disable button attribute to disable clear box.

--- textbox.js
+++ textbox.js
@@ -4,4 +4,5 @@ let inputs = document.querySelectorAll('textArea');
 
 btnClear.addEventListener('click',()=>{
 inputs.forEach(textArea => textArea.value='');
+btnClear.setAttribute('disabled', 'disabled'); // disables button after pressing clear
 });
